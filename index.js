2590dd3b41dda6e72534455688e208ab71a2b544
spartanhero93 <spartanhero93@gmail.com>
2021-09-16 23:43:46 -0400

    added mongodb integration, dotenv

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
+const { MongoClient } = require('mongodb')
+
 const PORT = 3004
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:<${process.env.DB_PASS}>@cluster0.mwk9e.mongodb.net/sample_training?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+client.connect(err => {
+  const collection = client.db('test').collection('devices')
+  console.log(collection)
+  // perform actions on the collection object
+  client.close()
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -33,23 +48,35 @@ const DB = [
     pictures: [
       'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/toyota-86-quick-review-lead-1594757313.jpg?crop=1xw:0.9948717948717948xh;center,top&resize=640:*'
     ]
+  },
+  {
+    carType: 'coupe',
+    make: 'Scion',
+    model: 'TC',
+    trim: 'standard',
+    year: '2013',
+    startingBid: '15000',
+    contactNumber: '7816664541',
+    color: 'black',
+    pictures: [
+      'https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/10q3/357232/2011-scion-tc-first-drive-review-car-and-driver-photo-360983-s-original.jpg?fill=2:1&resize=1200:*'
+    ]
   }
 ]
 
-app.post('/search', (req,res) => {
+app.post('/search', (req, res) => {
   console.log('user hit search')
   console.log(req.body)
-  res.send({data: true})
+  res.send({ data: true })
 })
 
 app.get('/explore', (req, res) => {
   console.log('user hit explore')
-  res.send(DB)
+  res.send({ data: DB })
 })
 
 app.get('/test', (req, res) => {
-  res.send({data: 'Hello dude!'})
+  res.send({ data: 'Hello dude!' })
 })
-
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
