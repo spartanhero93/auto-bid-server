6bba5612e210b728df53774a88e288a9dad8ebb5
spartanhero93 <spartanhero93@gmail.com>
2022-12-18 19:21:01 -0500

    removed mongoDB

--- index.js
+++ index.js
@@ -2,40 +2,60 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
-const { MongoClient } = require('mongodb')
-const mongoose = require('mongoose')
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mwk9e.mongodb.net/car_bid?retryWrites=true&w=majority`
 const PORT = 3004
 
-const carBidSchema = require('./models/CarBid.js')
-const CarBid = mongoose.model('carBid', carBidSchema, 'current_cars')
-
-
-async function main() {
-  await mongoose.connect(uri)
-  // const db = mongoose.connections
-
-  // let Luis = new User({ username: 'Booby', created: Date.now()})
-  // await Luis.save()
-  // console.log(Luis)
-
-  let newCarBid = new CarBid({
-    carType: 'sedan',
-    make: 'Honda',
-    model: 'Civic',
-    trim: 'touring',
-    year: '2019',
-    startingBid: '20000',
-    contactNumber: '7047778987',
-    color: 'Gray',
-    pictures: [
-      'https://upload.wikimedia.org/wikipedia/commons/1/19/Brazilian_Honda_Civic_touring_2017_%28cropped%29.jpg'
-    ]
-  })
-  await newCarBid.save()
-  console.log(newCarBid)
+class Car {
+  constructor (
+    carType,
+    make,
+    model,
+    trim,
+    year,
+    startingBid,
+    contactNumber,
+    color,
+    pictures
+  ) {
+    this.carType = carType
+    this.make = make
+    this.model = model
+    this.trim = trim
+    this.year = year
+    this.startingBid = startingBid
+    this.contactNumber = contactNumber
+    this.color = color
+    this.pictures = pictures
+  }
 }
-main().catch(err => console.log(err));
+
+// let newCarBid = {
+//   carType: 'sedan',
+//   make: 'Honda',
+//   model: 'Civic',
+//   trim: 'touring',
+//   year: '2019',
+//   startingBid: '20000',
+//   contactNumber: '7047778987',
+//   color: 'Gray',
+//   pictures: [
+//   'https://upload.wikimedia.org/wikipedia/commons/1/19/Brazilian_Honda_Civic_touring_2017_%28cropped%29.jpg'
+// ]
+// }
+// console.log(newCarBid)
+
+const honda = new Car(
+  'sedan',
+  'Honda',
+  'Civic',
+  'touring',
+  '2019',
+  '20000',
+  '7047778987',
+  'Gray',
+  ['https://upload.wikimedia.org/wikipedia/commons/1/19/Brazilian_Honda_Civic_touring_2017_%28cropped%29.jpg']
+)
+
+console.log(honda)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -43,13 +63,11 @@ app.use(cors())
 
 app.post('/search', (req, res) => {
   console.log('user hit search')
-  console.log(req.body)
-  res.send({ data: true })
 })
 
 app.get('/explore', (req, res) => {
   console.log('user hit explore')
-  res.send({ data: CarBid.findOne({}) })
+  res.send({data: [honda]})
 })
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
